f824614a3666c6099a5a13e735b150a8c660e4fa
xiangyuChen <xiangyu_975@163.com>
2021-03-24 18:49:45 +0800

    Docker使用

--- HelloDjango-blog-tutorial/blogproject/settings/common.py
+++ HelloDjango-blog-tutorial/blogproject/settings/common.py
@@ -67,11 +67,16 @@ WSGI_APPLICATION = 'blogproject.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # },
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'database' 'db.sqlite3'),
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
